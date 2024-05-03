4503d774c34ba2b0ccfb6def91903c926cc1a173
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-05-03 11:00:33 +0700

    Add process bar

---
+++ Assets/ManagerAudio/AudioManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] musicSound;
+    [SerializeField] private Sound[] sfxSound;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+
+    private static AudioManager instance;
+    public static AudioManager Instance => instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSound, x => x.nameClip == name);
+        if (s == null) return;
+        musicSource.clip = s.clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSound, x => x.nameClip == name);
+        if (s == null) return;
+        sfxSource.PlayOneShot(s.clip);
+    }
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        Debug.Log("Volume music is: " + musicSource.volume);
+    }
+
+    public void SFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+        Debug.Log("Volume SFX is: " + sfxSource.volume);
+    }
+
+
+    public float GetMusicVolume()
+    {
+        return musicSource.volume;
+    }
+
+    public float GetSFXVolume()
+    {
+        return sfxSource.volume;
+    }
+}
--- Assets/Scripts/HealthBar/HealthBarManager.cs
+++ Assets/Scripts/HealthBar/HealthBarManager.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading.Tasks;
+using System;
 
 public class HealthBarManager : MonoBehaviour
 {
     [SerializeField] private Slider processSlider;
+    [SerializeField][Range(0.01f, 0.5f)] private float offset;
+    [SerializeField][Range(1,10f)] private float maxTimeRequireToChangedColor;
+    
+    [SerializeField] private List<Image> nodeList = new List<Image>();
     private float maxTime = 0.25f;
+    private float speedRunProcess = 5f;
+    private bool canPressed = true;
+    private int indexNode = 0;
+    float timeRequireToChangedColor = 0;
+    
 
     private void Awake()
     {
@@ -15,11 +26,62 @@ public class HealthBarManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            processSlider.value += 5f * 0.02f;
-        }
-        processSlider.value -= Time.deltaTime * 0.2f;
+        ProcessBar();
     }
 
+
+    private void ProcessBar()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && canPressed) processSlider.value += speedRunProcess * 0.02f;
+        if (processSlider.value > 0.75f) speedRunProcess = 3f;
+        if (processSlider.value >= 1f && indexNode < nodeList.Count) Process();
+        else if (indexNode >= nodeList.Count) StartCoroutine(WaitingResetSliderBar());
+        processSlider.value -= Time.deltaTime * 0.3f;
+    }
+
+
+
+
+    private void Process()
+    {
+        timeRequireToChangedColor += Time.timeScale;
+        if (timeRequireToChangedColor >= maxTimeRequireToChangedColor)
+        {
+            ChangeColorNode(indexNode);
+            processSlider.value -= offset;
+            timeRequireToChangedColor = 0;
+        }
+        indexNode++;
+    }
+
+    private async void ChangeColorNode(int index)
+    {
+        await ChangeColorWithSound(index);
+    }
+
+    private async Task ChangeColorWithSound(int index)
+    {
+        nodeList[index].color = Color.green;
+        await Task.Delay(100); 
+        AudioManager.Instance.PlaySFX("Bip"); 
+    }
+
+
+    IEnumerator WaitingResetSliderBar()
+    {
+        canPressed = false;
+        indexNode = 0;
+        while (processSlider.value >0f)
+        {
+            processSlider.value -= 0.5f * Time.deltaTime;
+            yield return null;
+        }
+        foreach (var node in nodeList) node.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        speedRunProcess = 5f;
+        canPressed = true;
+    }
+
+   
+    
 }
